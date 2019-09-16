43cae5f36a89a4fa48115a55078b147de0f229b3
FIC <FIC@info.local>
2019-09-16 14:09:02 -0300

    aula de lógica, teste da idade

---
+++ aula2-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool entrada = false;
+            int idade;
+            string ret = "Entrada negada.";
+            Console.Write("Informe sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+            if(idade > 17)
+                entrada = true;
+
+            if(entrada == true)
+                ret = "Entrada autorizada.";            
+            Console.WriteLine(ret);
+        }
+    }
+}
---
+++ aulaLogica/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aulaLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1,n2,n3,media;
+            string res;
+            Console.WriteLine("Digite a 1º nota");
+            n1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a 2º nota");
+            n2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a 3º nota");
+            n3 = float.Parse(Console.ReadLine());
+            media = (n1+n2+n3)/3;
+            if(media >= 6){
+                res = "aprovado";
+            }
+            else if(media < 6 & media >= 4){
+                res = "recuperação";
+            }
+            else{
+                res="reprovado";
+            }
+            Console.WriteLine($"Este aluno possui média: {media} portanto sua situação é {res}.");
+        }
+    }
+}
