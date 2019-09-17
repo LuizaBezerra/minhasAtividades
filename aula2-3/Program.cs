a53ff508856cac5e6ffcf9aeb5cca645a190dc20
FIC <FIC@info.local>
2019-09-17 08:45:13 -0300

    update 17.09

--- aula2-3/Program.cs
+++ aula2-3/Program.cs
@@ -11,11 +11,8 @@ namespace aula2_3
             string ret = "Entrada negada.";
             Console.Write("Informe sua idade: ");
             idade = Convert.ToInt32(Console.ReadLine());
-            if(idade > 17)
-                entrada = true;
-
-            if(entrada == true)
-                ret = "Entrada autorizada.";            
+            if(idade > 17) entrada = true;
+            if(entrada == true) ret = "Entrada autorizada.";            
             Console.WriteLine(ret);
         }
     }
---
+++ exercicios/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("Digite sua idade");
+            idade = Convert.ToInt32(Console.ReadLine());
+            if(idade >18){
+                Console.WriteLine($"Classificação: Livre e Apenas adultos");
+            }
+            else if(idade == 17 ){
+                Console.WriteLine($"Classificação: Livre e Maduro");
+            }
+            else if(idade >= 13 && idade < 17 ){
+                Console.WriteLine($"Classificação: Livre e Adolescente");
+            }
+            else if(idade >= 10 && idade < 13 ){
+                Console.WriteLine($"Classificação: Livre e Infantil");
+            }
+            else{
+                 Console.WriteLine($"Classificação: Livre.");
+            }
+            
+        }
+    }
+}
